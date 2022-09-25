f3d777e1c7b7f5bafc9daa745e22add5d4094a06
Rahmadina <2011522012_rahmadina@student.unand.ac.id>
2022-09-25 18:08:44 +0700

    add list jadwal dosen&mhs, req, logbook

---
+++ app/src/main/java/com/example/ismobile/MyAdapter.java
@@ -0,0 +1,64 @@
+package com.example.ismobile;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<Bimbingan> bimbinganArrayList;
+    public MyAdapter(Context context, ArrayList<Bimbingan> bimbinganArrayList){
+        this.context = context;
+        this.bimbinganArrayList = bimbinganArrayList;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(context).inflate(R.layout.list_bimbingan,parent,false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Bimbingan bimbingan = bimbinganArrayList.get(position);
+        holder.bimbingan_nama.setText(bimbingan.nama);
+        holder.bimbingan_nim.setText(bimbingan.nim);
+        holder.bimbingan_ava.setImageResource(bimbingan.ava);
+    }
+
+    @Override
+    public int getItemCount() {
+        return bimbinganArrayList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ShapeableImageView bimbingan_ava;
+        TextView bimbingan_nama;
+        TextView bimbingan_nim;
+
+
+
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            bimbingan_ava = itemView.findViewById(R.id.bimbingan_ava);
+            bimbingan_nama = itemView.findViewById(R.id.bimbingan_nama);
+            bimbingan_nim = itemView.findViewById(R.id.bimbingan_nim);
+        }
+    }
+}
