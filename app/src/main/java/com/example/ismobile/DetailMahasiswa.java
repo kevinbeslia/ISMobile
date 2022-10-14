13e49ee1596951653da86ff4f3bf7b6f21132757
ruthemerald <ruthemeralda07@gmail.com>
2022-10-14 16:55:41 +0700

    + detail mahasiswa

---
+++ app/src/main/java/com/example/ismobile/DetailMahasiswa.java
@@ -0,0 +1,14 @@
+package com.example.ismobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailMahasiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_mahasiswa);
+    }
+}
